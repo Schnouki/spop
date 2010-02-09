b2ad01d5191559964ae7ec22506a9339273f6ce6
Thomas Jost <thomas.jost@gmail.com>
2010-02-09 23:19:42 +0100

    Daemon mode

--- src/main.c
+++ src/main.c
@@ -25,25 +25,29 @@
 #include "plugin.h"
 #include "session.h"
 
-int main(int argc, char** argv) {
-    printf("spop Copyright (C) " SPOP_YEAR " Thomas Jost\n"
-           "This program comes with ABSOLUTELY NO WARRANTY.\n"
-           "This is free software, and you are welcome to redistribute it under certain conditions.\n"
-           "See the COPYING file bundled with this program for details.\n"
+static const char* copyright_notice = 
+    "spop Copyright (C) " SPOP_YEAR " Thomas Jost\n"
+    "This program comes with ABSOLUTELY NO WARRANTY.\n"
+    "This is free software, and you are welcome to redistribute it under certain conditions.\n"
+    "See the COPYING file bundled with this program for details.\n"
+    "Powered by "
 #ifdef OPENSPOTIFY
-           "Powered by libopenspotify"
+    "libopenspotify"
 #else
-           "Powered by SPOTIFY(R) CORE"
+    "SPOTIFY(R) CORE"
 #endif
-           "\n\n");
+    "\n\n";
+
+static int run_as_daemon = 1;
+
+int real_main() {
+    const char* username;
+    const char* password;
 
     /* Init plugins */
     init_plugins();
 
     /* Read username and password */
-    const char* username;
-    const char* password;
-
     username = config_get_string("spotify_username");
     password = config_get_string("spotify_password");
 
@@ -57,4 +61,42 @@ int main(int argc, char** argv) {
     session_events_loop();
 
     return 0;
+}
+
+int main(int argc, char** argv) {
+    /* Parse command line options */
+    int opt;
+    while ((opt = getopt(argc, argv, "fh")) != -1) {
+        switch (opt) {
+        case 'f':
+            run_as_daemon = 0; break;
+        default:
+            printf("Usage: spopd [options\n"
+                   "Options:\n"
+                   "  -f        run in foreground (default: fork to background)\n"
+                   "  -h        display this message\n");
+            return 0;
+        }
+    }
+
+    if (!run_as_daemon) {
+        /* Stay in foreground: do everything here */
+        printf(copyright_notice);
+    }
+    else {
+        /* Run in daemon mode: fork to background */
+        pid_t pid = fork();
+        if (pid < 0) {
+            fprintf(stderr, "Error while forking process\n");
+            return 1;
+        }
+        else if (pid > 0) {
+            /* Parent process */
+            printf("spopd forked to background with pid %d\n", pid);
+            return 0;
+        }
+        /* The child process will continue and run the real_main() function */
+    }
+
+    return real_main();    
 }
