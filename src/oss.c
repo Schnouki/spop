9930a0a37b6e1ddd5b9c46ebf46b4aaf099c987b
Thomas Jost <thomas.jost@gmail.com>
2010-08-09 19:22:39 +0200

    Remove all that OSS cache and IO channel crap
    - revert to older version
    - add some debug messages
    - use a mutex since audio_delivery can now be called from session_play()

--- src/oss.c
+++ src/oss.c
@@ -17,46 +17,48 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <glib.h>
+#include <poll.h>
 #include <sys/soundcard.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/ioctl.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
 
 #include "audio.h"
 
-#define BUFSIZE  8192
-#define BUFNB    8
-
-typedef struct {
-    size_t size;
-    char* buf;
-} loss_buf;
-
-static GIOChannel* g_chan = NULL;
-static guint g_oss_ev_id = 0;
+static int g_oss_fd = -1;
+static struct pollfd g_pfd;
 static size_t g_oss_frame_size;
-
-static GQueue* g_free_bufs = NULL;
-static GQueue* g_full_bufs = NULL;
 static GStaticMutex g_oss_mutex = G_STATIC_MUTEX_INIT;
 
 /* "Private" functions, used to set up the OSS device */
-static void oss_open(const sp_audioformat* format) {
-    int fd, sample_type, i, tmp;
-    loss_buf* bufs = NULL;
-    GError* err = NULL;
-
-    g_debug("Opening OSS device");
-    g_static_mutex_lock(&g_oss_mutex);
-
+static void oss_open() {
     /* Open the device */
-    fd = open("/dev/dsp", O_WRONLY);
-    if (fd == -1)
+    g_debug("Opening OSS device");
+    g_oss_fd = open("/dev/dsp", O_WRONLY);
+    if (g_oss_fd == -1)
         g_error("Can't open OSS device: %s", g_strerror(errno));
 
+    /* Populate the pollfd struct used by poll() */
+    g_pfd.fd = g_oss_fd;
+    g_pfd.events = POLLOUT;
+}
+
+static void oss_close() {
+    g_debug("Closing OSS device");
+    if (close(g_oss_fd) == -1)
+        g_error("Can't close OSS device: %s", g_strerror(errno));
+    g_oss_fd = -1;
+}
+
+/* Set OSS parameters using "format" from libspotify */
+static void oss_setup(const sp_audioformat* format) {
+    int tmp;
+
     /* sample_type is an enum */
+    int sample_type;
     switch (format->sample_type) {
     case SP_SAMPLETYPE_INT16_NATIVE_ENDIAN:
         sample_type = AFMT_S16_NE;
@@ -71,143 +73,58 @@ static void oss_open(const sp_audioformat* format) {
        (http://manuals.opensound.com/developer/callorder.html) */
 
     tmp = format->channels;
-    if (ioctl(fd, SNDCTL_DSP_CHANNELS, &tmp) == -1)
+    if (ioctl(g_oss_fd, SNDCTL_DSP_CHANNELS, &tmp) == -1)
         g_error("Error setting OSS channels: %s", g_strerror(errno));
     if (tmp != format->channels)
         g_error( "Could not set OSS channels to %d (set to %d instead)", format->channels, tmp);
 
     tmp = sample_type;
-    if (ioctl(fd, SNDCTL_DSP_SETFMT, &tmp) == -1)
+    if (ioctl(g_oss_fd, SNDCTL_DSP_SETFMT, &tmp) == -1)
         g_error("Error setting OSS sample type: %s", g_strerror(errno));
     if (tmp != sample_type)
         g_error("Could not set OSS sample type to %d (set to %d instead)", sample_type, tmp);
 
     tmp = format->sample_rate;
-    if (ioctl(fd, SNDCTL_DSP_SPEED, &tmp) == -1)
+    if (ioctl(g_oss_fd, SNDCTL_DSP_SPEED, &tmp) == -1)
         g_error("Error setting OSS sample rate: %s", g_strerror(errno));
     /* Sample rate: the OSS doc that differences up to 10% should be accepted */
     if (((100*abs(format->sample_rate - tmp))/format->sample_rate) > 10)
         g_error("Could not set OSS sample rate to %d (set to %d instead)", format->sample_rate, tmp);
-
-    /* Create buffers */
-    if (!g_free_bufs) {
-        g_debug("Creating OSS buffers");
-
-        g_free_bufs = g_queue_new();
-        if (!g_free_bufs)
-            g_error("Can't allocate a queue of free buffers");
-        g_full_bufs = g_queue_new();
-        if (!g_full_bufs)
-            g_error("Can't allocate a queue of full buffers");
-
-        bufs = (loss_buf*) malloc(BUFNB*sizeof(loss_buf));
-        if (!bufs)
-            g_error("Can't allocate buffer structures");
-        for (i=0; i < BUFNB; i++) {
-            bufs[i].buf = malloc(BUFSIZE);
-            if (!(bufs[i].buf))
-                g_error("Can't allocate buffer");
-            g_queue_push_tail(g_free_bufs, &(bufs[i]));
-        }
-    }
-
-    /* Create a matching IO channel */
-    g_chan = g_io_channel_unix_new(fd);
-    if (!g_chan)
-        g_error("Could not create an IO channel for the OSS device");
-    if (g_io_channel_set_encoding(g_chan, NULL, &err) != G_IO_STATUS_NORMAL)
-        g_error("Could not set the encoding for the OSS IO channel: %s", err->message);
-
-    g_static_mutex_unlock(&g_oss_mutex);
-}
-
-static void oss_close() {
-    loss_buf* buf;
-    GError* err = NULL;
-
-    g_debug("Closing OSS device");
-    g_static_mutex_lock(&g_oss_mutex);
-
-    /* Flush the queue */
-    while (g_queue_get_length(g_full_bufs) > 0) {
-        buf = g_queue_pop_tail(g_full_bufs);
-        g_queue_push_tail(g_free_bufs, buf);
-    }
-
-    /* Remove the event source */
-    if ((g_oss_ev_id > 0) && !g_source_remove(g_oss_ev_id))
-        g_error("Can't remove OSS event source");
-
-    /* Shutdown the IO channel */
-    if (g_io_channel_shutdown(g_chan, FALSE, &err) != G_IO_STATUS_NORMAL)
-        g_error("Can't shutdown OSS IO channel: %s", err->message);
-    g_chan = NULL;
-
-    g_static_mutex_unlock(&g_oss_mutex);
-}
-
-/* OSS event -- write data from a buffer to the OSS device */
-static gboolean oss_event(GIOChannel* source, GIOCondition condition, gpointer data) {
-    GError* err = NULL;
-    loss_buf* buf = NULL;
-
-    g_static_mutex_lock(&g_oss_mutex);
-    buf = g_queue_pop_head(g_full_bufs);
-
-    if (buf) {
-        /* There is something to play */
-        if (g_io_channel_write_chars(source, buf->buf, buf->size, NULL, &err) != G_IO_STATUS_NORMAL)
-            g_error("Could not write to OSS device: %s", err->message);
-        
-        g_queue_push_tail(g_free_bufs, buf);
-        g_static_mutex_unlock(&g_oss_mutex);
-
-        return TRUE;
-    }
-    else {
-        /* Nothing to play: delete the event source */
-        g_oss_ev_id = 0;
-        g_static_mutex_unlock(&g_oss_mutex);
-        return FALSE;
-    }
 }
 
 /* "Public" function, called from a libspotify callback */
 int audio_delivery(const sp_audioformat* format, const void* frames, int num_frames) {
-    loss_buf* buf;
-    size_t size;
+    int ret;
 
+    g_static_mutex_lock(&g_oss_mutex);
+    
     /* What are we supposed to do here? */
     if (num_frames == 0) {
         /* Pause: close the device */
         oss_close();
-        return 0;
+        ret = 0;
     }
     else {
-        if (!g_chan)
+        if (g_oss_fd == -1) {
             /* Some frames to play, but the device is closed: open it and set it up */
-            oss_open(format);
-
-        /* Try to add data to the queue */
-        g_static_mutex_lock(&g_oss_mutex);
-        if (g_queue_get_length(g_free_bufs) == 0) {
-            /* Can't add anything */
-            g_static_mutex_unlock(&g_oss_mutex);
-            return 0;
+            oss_open();
+            oss_setup(format);
         }
 
-        /* Copy data to a free buffer and make it available to the player */
-        buf = g_queue_pop_head(g_free_bufs);
-        size = BUFSIZE < (num_frames * g_oss_frame_size) ? BUFSIZE : (num_frames * g_oss_frame_size);
-        memcpy(buf->buf, frames, size);
-        buf->size = size;
-        g_queue_push_tail(g_full_bufs, buf);
+        /* Is the device ready to be written to? */
+        ret = poll(&g_pfd, 1, 0);
+        if (ret == -1)
+            g_error("Can't poll OSS device: %s", g_strerror(errno));
+        else if (ret != 0) {
+            /* Ok, we can write to the device without blocking */
+            ret = write(g_oss_fd, frames, g_oss_frame_size * num_frames);
+            if (ret == -1)
+                g_error("Can't write to OSS device: %s", g_strerror(errno));
 
-        /* Add the event source if it's not here already */
-        if (g_oss_ev_id == 0)
-            g_oss_ev_id = g_io_add_watch_full(g_chan, G_PRIORITY_HIGH, G_IO_OUT|G_IO_ERR|G_IO_NVAL, oss_event, NULL, NULL);
-
-        g_static_mutex_unlock(&g_oss_mutex);
-        return size / g_oss_frame_size;
+            ret /= g_oss_frame_size;
+        }
     }
+
+    g_static_mutex_unlock(&g_oss_mutex);
+    return ret;
 }
